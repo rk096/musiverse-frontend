1d9bb85e2d28efe90a8a6ed32f7a43a6a6164e0e
richa <richakamani32@gmail.com>
2023-08-19 16:40:14 -0400

    add input fields in login

---
+++ src/components/shared/TextInput.js
@@ -0,0 +1,24 @@
+const TextInput = ({
+    label,
+    placeholder,
+    className
+}) => {
+    return (
+        <div
+             className={`textInputDiv flex flex-col space-y-2 w-full ${className}`}
+        >
+            <label for={label}>
+                {label}
+            </label>
+            <input
+                type="text"
+                placeholder={placeholder}
+                className="p-3 border border-gray-400 border-solid rounded placeholder-gray-500"
+                id={label}
+            />
+        </div>
+
+    );
+};
+
+export default TextInput;
